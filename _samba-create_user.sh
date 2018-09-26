7387f202c76b88b93a1ce31822eff888ebd02f88
chernic <843399943@qq.com>
2018-09-26 10:20:14 +0800

    Add chernic:chernic

--- _samba-create_user.sh
+++ _samba-create_user.sh
@@ -16,14 +16,17 @@ smbclient -L //192.168.1.101 -Ufocustar
 
 
 groupadd focustar
-
 useradd focustar -g focustar
 # useradd：focustar 组已经存在 - 如果您想将此用户加入到该组，请使用 -g 参数。
-
 pdbedit -a -u focustar
-
 chmod 766 /home/focustar
 
+
+groupadd chernic
+useradd chernic -g chernic
+# useradd：focustar 组已经存在 - 如果您想将此用户加入到该组，请使用 -g 参数。
+pdbedit -a -u chernic
+chmod 766 /home/chernic
 
 
 # ============================================================================================================
