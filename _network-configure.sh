c52a84f340b25b34a369a856c6373f04f0efc800
chernic <843399943@qq.com>
2018-09-29 11:29:04 +0800

    # @Chernic : linux基本网络配置, 给初装的机器配置使用, 常手动配置 # @changelog ## 2018-09-29 Chernic <chernic AT qq.com> #- 增加配置前实现检IP未被占用 #- 增加一些大纲式注释

--- _network-configure.sh
+++ _network-configure.sh
@@ -2,6 +2,9 @@
 
 # @Chernic : linux基本网络配置, 给初装的机器配置使用, 常手动配置
 # @changelog
+## 2018-09-29 Chernic <chernic AT qq.com>
+#- 增加配置前实现检IP未被占用
+#- 增加一些大纲式注释
 ## 2018-09-13 Chernic <chernic AT qq.com>
 #- 强调(必须修改项目)
 ## 2018-09-12 Chernic <chernic AT qq.com>
@@ -12,21 +15,32 @@
 echo "you maight not run this but just have a look"
 exit 
 
+#########################################################
 # 硬件-网络连接-桥接模式(√)
 
-需要按实际情况修改 
+#########################################################
+# 尝试ping 看地址是否被占用
+ping 192.168.2.180
+
+#########################################################
+# 需要按实际情况修改 
 DEVICE 
 HWADDR 
 NETMASK
 IPADDR
-而且根据本公司环境必须配置
+
+#########################################################
+# 而且根据本公司环境必须配置
 GATEWAY   (必须)
 DNS1      (必须)
 DNS2      (必须)
 
+#########################################################
+# 检验配置是否成功
 chkconfig network on
 service network restart
 ping mirrors.163.com
+
 
 /etc/sysconfig/network
 NETWORKING=yes
@@ -41,7 +55,7 @@ UUID=04f18845-86b3-4ac2-bd6b-ca9a3d8e534f
 ONBOOT=yes
 NM_CONTROLLED=yes
 BOOTPROTO=static
-IPADDR=192.168.2.197
+IPADDR=192.168.2.180
 NETMASK=255.255.255.0
 IPV6INIT=no
 DNS1=192.168.2.1
