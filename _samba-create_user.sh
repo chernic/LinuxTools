0abe8bca935687b0c3184dd6041a6290dfe6db42
chernic <843399943@qq.com>
2018-10-19 16:35:30 +0800

    ## 2018-10-19 Chernic <chernic AT qq.com> #- 免密码输入

--- _samba-create_user.sh
+++ _samba-create_user.sh
@@ -3,6 +3,7 @@
 # @Chernic : 为samba增加用户
 # @changelog
 ## 2018-10-19 Chernic <chernic AT qq.com>
+#- 免密码输入
 #- chernic 加到focustar用户组(优化)
 #- 隐藏文件
 ## 2018-09-26 Chernic <chernic AT qq.com>
@@ -13,24 +14,22 @@
 yum install samba
 yum install samba-client
 
-smbclient -L //192.168.1.101 -Ufocustar
+# smbclient -L //192.168.1.101 -Ufocustar
 # Enter focustar's password: 
 # session setup failed: NT_STATUS_LOGON_FAILURE
 
-
 groupadd focustar
-useradd -g focustar focustar        # 组已经存在 - 如果您想将此用户加入到该组，请使用 -g 参数。
+useradd -g    focustar focustar     # 组已经存在 - 如果您想将此用户加入到该组，请使用 -g 参数。
 usermod -a -G focustar focustar     # 把用户添加进入某个组(s）
-pdbedit -a -u focustar              # samba 添加用户
 chmod 776 /home/focustar
 
 groupadd chernic
-useradd -g chernic  chernic         # 组已经存在 - 如果您想将此用户加入到该组，请使用 -g 参数。
-usermod -a -G focustar chernic      ## 把用户添加进入某个组(s）
-pdbedit -a -u chernic               # samba 添加用户
-chmod 776 /home/chernic
+useradd -g    chernic  chernic      # 组已经存在 - 如果您想将此用户加入到该组，请使用 -g 参数。
+usermod -a -G focustar chernic      # 把用户添加进入某个组(s）
 
-[ -f /etc/samba/smb.conf ] && mv /etc/samba/smb.conf /etc/samba/smb.conf.bak
+chmod 777 /home/chernic
+
+[ -f /etc/samba/smb.conf ] && mv /etc/samba/smb.conf /etc/samba/smb.conf.bak.$(date "+%Y%m%d_%H:%M:%S")
 
 cat >> /etc/samba/smb.conf << EOF
 # This is the main Samba configuration file. You should read the
@@ -328,7 +327,7 @@ cat >> /etc/samba/smb.conf << EOF
 	browseable = yes
 	writable = yes
 	veto files=/.*/
-;	valid users = %S
+;	valid users = @focustar focustar
 ;	valid users = MYDOMAIN\%S
 
 [chernic]
@@ -337,41 +336,23 @@ cat >> /etc/samba/smb.conf << EOF
 	browseable = yes
 	writable = yes
 	veto files=/.*/
-;	valid users = %S
+;	valid users = @focustar focustar
 ;	valid users = MYDOMAIN\%S
 
 EOF
 
-# ============================================================================================================
-# ============================================================================================================
-# Dependencies Resolved
-# ============================================================================================================
- # Package                           Arch               Version                        Repository        Size
-# ============================================================================================================
-# Installing:
- # samba                             x86_64             3.6.23-51.el6                  base             5.1 M
-# Installing for dependencies:
- # avahi-libs                        x86_64             0.6.25-17.el6                  base              55 k
- # cups-libs                         x86_64             1:1.4.2-79.el6                 base             323 k
- # gnutls                            x86_64             2.12.23-22.el6                 base             389 k
- # libjpeg-turbo                     x86_64             1.2.1-3.el6_5                  base             174 k
- # libpng                            x86_64             2:1.2.49-2.el6_7               base             182 k
- # libtalloc                         x86_64             2.1.5-1.el6_7                  base              26 k
- # libtdb                            x86_64             1.3.8-3.el6_8.2                base              40 k
- # libtevent                         x86_64             0.9.26-2.el6_7                 base              29 k
- # libtiff                           x86_64             3.9.4-21.el6_8                 base             346 k
- # perl                              x86_64             4:5.10.1-144.el6               base              10 M
- # perl-Module-Pluggable             x86_64             1:3.90-144.el6                 base              41 k
- # perl-Pod-Escapes                  x86_64             1:1.04-144.el6                 base              33 k
- # perl-Pod-Simple                   x86_64             1:3.13-144.el6                 base             213 k
- # perl-libs                         x86_64             4:5.10.1-144.el6               base             579 k
- # perl-version                      x86_64             3:0.77-144.el6                 base              52 k
- # samba-common                      x86_64             3.6.23-51.el6                  base              10 M
- # samba-winbind                     x86_64             3.6.23-51.el6                  base             2.2 M
- # samba-winbind-clients             x86_64             3.6.23-51.el6                  base             2.0 M
+#   [root@tas-compile home]# pdbedit -h
+#   Usage: [OPTION...]
+### -t, --password-from-stdin          get password from standard in
+user_passwd=focustar
+echo ""
+echo "------- 增加 samba 用户 focustar  并设置密码 focustar"
+echo -e "$user_passwd\n$user_passwd" | pdbedit -t -a -u focustar
 
-# Transaction Summary
-# ============================================================================================================
-# Install      19 Package(s)
+echo ""
+echo "------- 增加 samba 用户 chernic  并设置密码 focustar"
+echo -e "$user_passwd\n$user_passwd" | pdbedit -t -a -u chernic
 
-# Total download size: 32 M
+echo ""
+echo "------- 列出所有 samba 用户"
+pdbedit -L
